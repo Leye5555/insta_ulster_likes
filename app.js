da0ebd24744195c5542738d729e45b1114b86500
Olubummo Faith <59509795+Leye5555@users.noreply.github.com>
2025-04-23 11:45:14 +0100

    fix: cors config

--- app.js
+++ app.js
@@ -9,23 +9,31 @@ if (process.env.NODE_ENV !== "production") {
 }
 const PORT = process.env.PORT || 8000;
 const app = express();
-cors.config({
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-});
 
-app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(router);
 
-db()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("failed to connect", err);
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// db()
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("failed to connect", err);
+//   });
