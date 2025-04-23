3d80e22b477adbcbac22c06568c02c17fe627c15
Olubummo Faith <59509795+Leye5555@users.noreply.github.com>
2025-04-23 15:03:14 +0100

    fix: production environment case sensitivity

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello from the server");
+  res.status(200).send("Hello from the server");
+});
+
+router.get("/login", (req, res) => {
+  res.status(200).json({
+    name: "test",
+    age: 24,
+  });
 });
 
 module.exports = router;
